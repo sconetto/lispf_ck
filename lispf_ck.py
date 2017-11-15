45fae0561cb832609117ea83fbde3485cf48a064
João Pedro Sconetto <sconetto.joao@gmail.com>
2017-11-15 19:36:39 -0200

    Minor changes

--- lispf_ck.py
+++ lispf_ck.py
@@ -1,38 +1,52 @@
 import ox
+import click
 
-lexer = ox.make_lexer([
-    ('NUMBER', r'\d+(\.\d*)?'),
-    ('LPAR', r'\('),
-    ('RPAR', r'\)'),
-    ('DEF_FUNC', r'def'),
-    ('EXEC_BLOCK', r'do'),
-    ('LOOP', r'loop'),
-    ('PRINT', r'print'),
-    ('READ', r'read'),
-    ('DEC', r'dec'),
-    ('INC', r'inc'),
-    ('LEFT', r'left'),
-    ('RIGHT', r'right'),
-    ('DO_BEFORE', r'do\-before'),
-    ('DO_AFTER', r'do\-after'),
-    ('ADD', r'add'),
-    ('SUB', r'sub'),
-    ('COMMENT', r'\;([^\n\r]+)'),
-    ('NAME', r'[a-zA-z]+\d*'),
-])
+@click.command()
+@click.argument('lispf_ck',type=click.File('r'))
 
-token_list = ['NUMBER', 'LPAR', 'RPAR', 'DEF_FUNC', 'EXEC_BLOCK', 'LOOP',
-              'PRINT', 'READ', 'DEC', 'INC', 'LEFT', 'RIGHT', 'DO_BEFORE',
-              'DO_AFTER', 'ADD', 'SUB', 'COMMENT', 'NAME']
+def ast_lispf_fc(lispf_ck):
+    lexer = ox.make_lexer([
+        ('NUMBER', r'\d+(\.\d*)?'),
+        ('LPAR', r'\('),
+        ('RPAR', r'\)'),
+        ('DEF_FUNC', r'def'),
+        ('EXEC_BLOCK', r'do'),
+        ('LOOP', r'loop'),
+        ('PRINT', r'print'),
+        ('READ', r'read'),
+        ('DEC', r'dec'),
+        ('INC', r'inc'),
+        ('LEFT', r'left'),
+        ('RIGHT', r'right'),
+        ('DO_BEFORE', r'do\-before'),
+        ('DO_AFTER', r'do\-after'),
+        ('ADD', r'add'),
+        ('SUB', r'sub'),
+        ('COMMENT', r'\;([^\n\r]+)'),
+        ('NAME', r'[a-zA-z]+\d*'),
+    ])
 
-parser = ox.make_parser ([
+    token_list = ['NUMBER', 'LPAR', 'RPAR', 'DEF_FUNC', 'EXEC_BLOCK', 'LOOP',
+                  'PRINT', 'READ', 'DEC', 'INC', 'LEFT', 'RIGHT', 'DO_BEFORE',
+                  'DO_AFTER', 'ADD', 'SUB', 'COMMENT', 'NAME']
 
-], token_list)
+    parser = ox.make_parser ([
 
-lisp_entry = input('Enter the lisp code: ')
+    ], token_list)
 
-tokens = lexer(lisp_entry)
-print('Tokens:\n', tokens)
+    source = lispf_ck.read()
 
-ast = parser(tokens)
-print('AST:\n', ast)
+    lisp_entry = []
+    for data in source:
+        lisp_entry.append(data)
+
+
+    tokens = lexer(lisp_entry)
+    print('Tokens:\n', tokens)
+
+    ast = parser(tokens)
+    print('AST:\n', ast)
+
+
+if __name__ == "__main__":
+    run = ast_lispf_fc()
